32ec1e374f32a9e43fe860adee24639cee63ef09
major-welc <major.welc@gmail.com>
2014-04-20 23:51:01 +0200

    Definition style pour tablette (landscape & not)

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,13 @@
 var gameState = "start";
 var PLAYGROUND_HEIGHT;
 var PLAYGROUND_WIDTH;
+var LANDSCAPE=false;
+/*
+*   unique_onclick permet de bloquer la definition des onclick lors du switch dans la main
+*   et eviter l'envoi de plusieurs changeScreen() si l'utilisateur n'appuie pas instantanement
+*   sur le bouton pour changer de menu.
+*/
+var unique_onclick=0;
 
 function main()
 {
@@ -22,19 +29,22 @@ function main()
             
     case "start" :
         PLAYGROUND_HEIGHT = $("#playground").height();
-        PLAYGROUND_WIDTH = $("#playground").width();  
+        PLAYGROUND_WIDTH = $("#playground").width();
+        if(PLAYGROUND_WIDTH>PLAYGROUND_HEIGHT){LANDSCAPE=true;}
         changeScreen("title");
         break;
 
                
 	case "title" :
-        
-        $("#quittitle").click(function (){
-            $("#menuTitle").remove();
-            changeScreen("menu");
-            console.log("click exit title screen");
-        });
-            
+        if (unique_onclick==0){
+            unique_onclick++;
+            $("#quittitle").click(function (){
+                $("#menuTitle").remove();
+                console.log("click exit title screen");
+                unique_onclick--;
+                changeScreen("menu");
+            });
+        }
 		break;
 	
 		
@@ -45,10 +55,15 @@ function main()
             
         $("#bmSizeDisplay").text($("input[name=bmSize]").val()+" bits");
         
-        $("#launchIt").click(function (){
-            console.log("click exit menu");
-            $("#menuGame").remove();
-        });   
+        if(unique_onclick==0){
+            unique_onclick++;
+            $("#launchIt").click(function (){
+                console.log("click exit menu");
+                $("#menuGame").remove();
+                unique_onclick++;
+                changeScreen("game");
+            });   
+        }
 		break;
 			
 	case "game" :
@@ -81,23 +96,12 @@ function changeScreen(screen)
     	switch(gameState)
 	{
 	case "title" :
-      /*  if(!m_title){
-            console.log("writing title section");
-            $("body").addClass("titlescreen");
-            $("#playground").prepend('<div class="title customfont" id="titlename">Fat Game</div><p class="customfont animated infinite pulse" id="quittitle">click here to start!</p>');
-            $("#titlename").y($(window).height*0.15);
-            m_title=true;
-            m_menu=false;
-            m_game=false;
-            m_gameOver=false;
-            m_pause=false;
-        } */
-        
+            
         console.log("writing title section");
-        $.playground().addGroup("menuTitle",{width: 500 , height: 300 });
+        $.playground().addGroup("menuTitle",{width: 400 , height: 300 });
         $("#menuTitle").y((PLAYGROUND_HEIGHT/3)-$("#menuTitle").height()/2);
         $("#menuTitle").x((PLAYGROUND_WIDTH/2)-$("#menuTitle").width()/2);
-        
+
         $("#menuTitle").append('<div class="title customfont" id="titlename">Fat Game</div>'+
                                '<p class="customfont animated infinite pulse" id="quittitle">click here to start!</p>'
                              );
@@ -107,23 +111,36 @@ function changeScreen(screen)
 	case "menu" :
         
         console.log("writing menu section");
-        $.playground().addGroup("menuGame",{width: PLAYGROUND_WIDTH/1.5 , height: PLAYGROUND_HEIGHT/2 });
+        if(LANDSCAPE){  
+            $.playground().addGroup("menuGame",{width: PLAYGROUND_WIDTH*0.55, height: PLAYGROUND_HEIGHT*0.4 });
+            $("#menuGame").y((PLAYGROUND_HEIGHT/2)-($("#menuGame").height()/1.1));
+        }
+        else {
+            $.playground().addGroup("menuGame",{width: PLAYGROUND_WIDTH*0.5, height: PLAYGROUND_HEIGHT*0.7 });
+            $("#menuGame").y((PLAYGROUND_HEIGHT/2)-($("#menuGame").height()/2));
+        }
         $("#menuGame").prepend('<div id="popNewGame" class="customfont menu">'+
-                                 '<div class="startOption">FAT type<input type="range" min="12" max="20" step="4" name="fType" align="left"></input>'+
+                                 '<div class="startOption"><div style="margin-bottom:-5px">FAT type</div>'+
+                                    '<input type="range" min="12" max="20" step="4" name="fType" align="left"></input>'+
                                     '<div id="fTypeDisplay"></div></div>'+
-                                 '<div class="startOption">block max size<input type="range" name="bmSize" align="left"></input><div id="bmSizeDisplay"></div></div>'+
-                                 '<div id="launchIt" class="animated infinite flash2"><p>launch game</p></div></div>'
+                                 '<div class="startOption"><div style="margin-bottom:-5px">block max size</div>'+
+                                    '<input type="range" name="bmSize" min="64" max="2048" step="64" align="left"></input><div id="bmSizeDisplay"></div></div>'+
+                                 '<div id="launchIt" class="animated infinite flash2" style="margin-bottom:0px"><p>launch game</p></div></div>'
                                 );
         $("#popNewGame div:last-child").css("margin-bottom","30px");
         $("#popNewGame div:first-child").css("margin-top","50px").css("margin-bottom","20px");
-        $("#menuGame").x((PLAYGROUND_WIDTH/2)-($("#menuGame").width()/4));
-        $("#menuGame").y((PLAYGROUND_HEIGHT/2)-($("#menuGame").height()/1.5));
+        $("#menuGame").x((PLAYGROUND_WIDTH/2)-($("#menuGame").width()/2));
+        
             
             
 		break;
 			
 	case "game" :
 		
+            $.playground().addGroup("menuInGame",{width: 40, height: 50 });
+            $("#menuInGame").prepend('<div class="customfont pauseButton"><div id="pauseIt"><font size="3em">||</font></div></div>');
+            $("#pauseIt").css("margin-top","5px");
+            $("#pauseIt").css("margin-bottom","5px");
 		break;
 		
 	case "pause" :
